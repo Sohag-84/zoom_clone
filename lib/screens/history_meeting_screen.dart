abb0f9155f0891ceccff282b1c09758f9d0ab6a3
IH Sohag <injamulhaqsohag19977@gmail.com>
2022-04-17 02:08:42 +0600

    retrieve data from the firestore and show meeting id and date in the meetings screen

--- lib/screens/history_meeting_screen.dart
+++ lib/screens/history_meeting_screen.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:zoom_clone/resources/firestor_methods.dart';
 
 class HistoryMeetingScreen extends StatelessWidget {
-  const HistoryMeetingScreen({ Key? key }) : super(key: key);
+  const HistoryMeetingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return StreamBuilder(
+      stream: FirestoreMethods().meetingsHistory,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView.builder(
+          itemCount: (snapshot.data! as dynamic).docs.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                  "Meeting Id: ${(snapshot.data! as dynamic).docs[index]['meetingName']}"),
+              subtitle: Text(
+                  "Joined on ${DateFormat.yMMMd().format((snapshot.data! as dynamic).docs[index]['createdAt'].toDate())}"),
+            );
+          },
+        );
+      },
     );
   }
 }
